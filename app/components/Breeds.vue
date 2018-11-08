34311358028d264c3f104856b01da4d37564c68a
Rodrigo Nascimento de Carvalho <rnascimentodecarva@unomaha.edu>
2018-11-08 19:25:09 -0200

    -Making some adjustments to layout. -Fixing issue with initial loading.

--- app/components/Breeds.vue
+++ app/components/Breeds.vue
@@ -1,15 +1,25 @@
 <template>
     <Page>
-        <ActionBar title="Select your breed:"/>
-        <FlexboxLayout flexDirection="column">
-            <ActivityIndicator height="10%" :busy="loading" @busyChange="test"/>
-            <ListPicker height="80%" :items="breedList"
-                        selectedIndex=""
-                        @selectedIndexChange="onSelectionChange"
-                        v-if="!loading"
-                />
-            <Button height="10%" text="Button" @tap="onButtonTap" />
-        </FlexboxLayout >
+        <ActionBar title="Doggy Pic" flat="true"/>
+        <DockLayout stretchLastChild="true" >
+            <StackLayout class="context-section" dock="top" verticalAlignment="middle" height="7%">
+                 <Gradient direction="to bottom" colors="#68a182, #3c875e">
+                    <Label  text="Select your breed:" class="context-text"/>
+                 </Gradient>
+            </StackLayout>
+            <Gradient direction="to right" colors="#ff6e92, #ffa36d, #ff6e92">
+                <FlexboxLayout flexDirection="column" v-cloak>
+                    <ActivityIndicator height="100%" :busy="loading" v-show="loading"/>
+                        <ListView height="100%" for="breed in breedList" v-if="breedList.length" separatorColor="#ffe2e2">
+                            <v-template>
+                                <StackLayout>
+                                    <Label class="doggo" :text="breed" />
+                                </StackLayout>
+                            </v-template>
+                        </ListView>
+                </FlexboxLayout>
+            </Gradient>
+        </DockLayout>
     </Page>
 </template>
 
@@ -20,7 +30,7 @@
         data() {
             return {
                 breedList: [],
-                loading: false
+                loading: true
             }
         },
         methods: {
@@ -37,8 +47,7 @@
                 this.loading = !this.loading;
             }
         },
-        mounted() {
-            this.setLoading(true)
+        created() {
             /**
              * - Calls Dog CEO API
              * - Data will be in the form:
@@ -46,7 +55,6 @@
              * - We want something like:
              * [ "Affenpinscher", "African", ... ]
              */
-
             http.getJSON('https://dog.ceo/api/breeds/list/all').then(data => {
                     /**
                      * Object.keys transforms:
@@ -61,16 +69,51 @@
                 .catch(err => {
                     console.error('There was an error:', err)
                 })
-                .finally(
+                .finally(() => {
                     this.setLoading(false)
-                );
+                });
         },
     }
 </script>
 
 <style scoped>
+    [v-cloak] {
+        display: none;
+    }
+
     ActionBar {
-        background-color: #53ba82;
+        background-image: linear-gradient(to bottom, #53ba82, #68a182);
         color: #ffffff;
+    }
+    .context-section {
+        background-color: #53ba82;
+    }
+
+    .context-section .context-text {
+        width: 100%;
+        height: 100%;
+        padding: 15px 30px;
+        font-size: 20;
+        font-weight: 700;
+        color: #ffffff;
+    }
+
+    ListView .doggo {
+        font-size: 20px;
+        font-weight: 500;
+        color: #fff0e1;
+        text-align: center;
+        padding: 50px;
+    }
+
+    FlexboxLayout {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    ActivityIndicator {
+        width: 20%;
+        height: auto;
     }
 </style>
